7389fea6255220fd9aed1bba30cd6b907db371e0
Thomas VISENTIN <tvisenti@e3r9p17.42.fr>
2015-12-04 20:24:41 +0100

    OK

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: tvisenti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/28 15:23:25 by tvisenti          #+#    #+#             */
-/*   Updated: 2015/12/03 09:47:38 by tvisenti         ###   ########.fr       */
+/*   Updated: 2015/12/04 18:55:36 by tvisenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,29 @@
 
 size_t		ft_strlcat(char *dst, const char *src, size_t size)
 {
-	dst = NULL;
-	src = NULL;
-	size = 0;
-	return (0);
+	char	*pdst;
+	char	*psrc;
+	size_t	n;
+	size_t	len;
+
+	pdst = dst;
+	psrc = (char *)src;
+	n = size;
+	while (*pdst != '\0' && n-- != 0)
+		pdst++;
+	len = pdst - dst;
+	n = size - len;
+	if (n == 0)
+		return (len + ft_strlen(src));
+	while (*psrc != '\0')
+	{
+		if (n != 1)
+		{
+			*pdst++ = *psrc;
+			n--;
+		}
+		psrc++;
+	}
+	*pdst = '\0';
+	return (len + (psrc - src));
 }
