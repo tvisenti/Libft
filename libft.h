a98a1214884eef788653793b7d1244c17130771e
tvisenti <tho.visentin@gmail.com>
2016-01-07 09:24:46 +0100

    Ajout de ft_strclen

--- libft.h
+++ libft.h
@@ -88,6 +88,7 @@ char				*ft_str_delwhite(char *str);
 void				ft_swap(int *a, int *b);
 int					ft_isupper(int c);
 int					ft_islower(int c);
+size_t				ft_strclen(char *str, char c);
 char				*ft_strocpy(char *str, char c);
 
 #endif
