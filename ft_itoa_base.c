ca1ca01f2096cd5ec02b7ffbc35af6daaa7e6486
Visentin <tvisenti@42.student.fr>
2016-04-19 14:10:11 +0200

    modif itoa base

--- ft_itoa_base.c
+++ ft_itoa_base.c
@@ -14,32 +14,35 @@
 #include <unistd.h>
 #include "libft.h"
 
-char            *ft_itoa_base(int value, int base)
+char	*ft_itoa_base(int value, int base)
 {
-  unsigned int  nb;
-  char          ref[16] = {"0123456789ABCDEF"}
-  int           len = 0;
-  int           isneg = 0;
-  char          *res;
+	char base_digits[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+	int converted_number[64];
+	int i = 0;
+	int n;
+	char *result;
 
-  nb = value < 0 ? -value : value;
-  if (value < 0 && base == 10)
-    isneg = 1;
-  while (value != 0)
-  {
-    value != base;
-    len++;
-  }
-  if (nb == 0)
-    len = 1;
-  res = malloc(len + isneg + 1);
-  res[len + isneg] = '\0';
-  while (len--)
-  {
-    res[len + isneg] = ref[nb % base];
-    nb /= base;
-  }
-  if (isneg)
-    res[0] = '-';
-    return (res);
+	n = value;
+	if (value < 0)
+		value = value * -1;
+	while (value != 0)
+	{
+		converted_number[i] = value % base;
+		value = value / base;
+		++i;
+	}
+	--i;
+	result = malloc(i * sizeof(char));
+	if (n < 0)
+	{
+		result[0] = '-';
+		n = 1;
+	}
+	while (i >= 0)
+	{
+		result[n] = base_digits[converted_number[i]];
+		i--;
+		n++;
+	}
+	return (result);
 }
